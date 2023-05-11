1ee3f578d40c0a06beaff25f8a4bfed4ba15a986
Camilo1914z <camilo1914z@gmail.com>
2023-05-11 10:24:22 -0500

    PayMode-Index,Create,edit,Delet

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,7 +12,10 @@ namespace SupermarketWEB.Models
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public ICollection<Category>? categories { get; set; }
+        
+
+        
 
 
     }
--- Pages/Products/Index.cshtml.cs
+++ Pages/Products/Index.cshtml.cs
@@ -9,16 +9,19 @@ namespace SupermarketWEB.Pages.Products
     public class IndexModel : PageModel
     {
         private readonly SupermarketContext _context;
+
         public IndexModel(SupermarketContext context)
         {
             _context = context;
         }
+
+
         public IList<Product> Products { get; set; } = default!;
+
         public async Task OnGetAsync()
         {
             if (_context.Products != null)
             {
-
                 Products = await _context.Products.ToListAsync();
             }
         }
